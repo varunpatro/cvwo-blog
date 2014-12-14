3da878dae73ba85383afbda0da0cafd0155d8137
Varun Patro <varunpatro@u.nus.edu>
2014-12-14 20:10:21 +0530

    Add style.css, auth.php

---
+++ auth.php
@@ -0,0 +1,31 @@
+<?php
+$servername = "localhost";
+$username = "varun";
+$password = "house";
+$dbname = "myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+	echo "Connection Succesfull!";
+} 
+
+$sql = "SELECT * FROM MyGuests";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+	echo "<table><tr><th>ID</th><th>Name</th></tr>";
+	//output data of each row
+	while ($row = $result->fetch_assoc()) {
+		echo "<tr><td>".$row["id"]."</td><td>".$row["firstname"]." ".$row["lastname"]."</td></tr>";
+	}
+	echo "</table>";
+} else {
+	echo "0 results";
+}
+
+$conn->close();
+?>
