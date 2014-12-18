1c0412747a8969e5bc3449b5d0ae6c48cb3eaa06
Varun Patro <varunpatro@u.nus.edu>
2014-12-18 21:08:00 +0530

    Initial Setup of all_articles page

--- all_articles.php
+++ all_articles.php
@@ -1,8 +1,23 @@
+<?php
+session_start();
+?>
 <html>
-<head>
-	<meta charset="utf-8">
-	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-	<title>All Articles</title>
-</head>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="/css/bootstrap.min.css">
+		<title>All Articles</title>
+	</head>
+	<body>
+	<?php require_once("nav.php"); ?>
+		<div class="container">
+			<?php
+			require_once("conn.php");
 
+			$sql_query = "SELECT * FROM articles;";
+			$articles = $conn->$sql_query;
+
+			
+			?>
+		</div>	
+	</body>
 </html>
