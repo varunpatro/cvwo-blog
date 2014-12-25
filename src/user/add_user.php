f1b0f4a28d5a61d33b0a02eec92263631814499c
Varun Patro <varunpatro@u.nus.edu>
2014-12-25 12:29:28 +0530

    Add validation for all forms

--- src/user/add_user.php
+++ src/user/add_user.php
@@ -2,13 +2,27 @@
 
 require_once("../config/db_config.php");
 
+$uname_query = login_query($_POST['username']);
 
-add($_POST['username'], $_POST['password'], $_POST['name']);
+if ($uname_query->num_rows > 0) {
+	$_SESSION["message"] = "{$_POST['username']} has already been taken. Please enter another one.";
+	$_SESSION['temp_name'] = $_POST['name'];
+	header("Location: /views/signup_page.php");
+} else if ($_POST['password'] !== $_POST['re-password']) {
+	$_SESSION["message"] = "Passwords do not match. Please try again.";
+	$_SESSION['temp_name'] = $_POST['name'];
+	$_SESSION['temp_username'] = $_POST['username'];
+	header("Location: /views/signup_page.php");
+} else {
+	add($_POST['username'], $_POST['password'], $_POST['name']);
 
-$_SESSION["logged_in"] = true;
-$_SESSION["username"] = $_POST["username"];
-$_SESSION["name"] = $_POST["name"];
+	$_SESSION["logged_in"] = true;
+	$_SESSION["username"] = $_POST["username"];
+	$_SESSION["name"] = $_POST["name"];
 
-header("Location: /config/message_passing.php?m=add_user");
+	header("Location: /config/message_passing.php?m=add_user");
+}
+
+
 
 ?>
--- src/views/signup_page.php
+++ src/views/signup_page.php
@@ -5,12 +5,13 @@ require_once("../config/init.php");
 	<body>
 		<?php 
 		require_once("../views/common/navbar.php"); ?>
+		<p> <?php echo $_SESSION['message']; $_SESSION['message'] = "" ?>
 		<p>
 			<form class="form-signin" role="form" action="/user/add_user.php" method="POST">
 				<fieldset>
 					<legend><h3 class="form-signin-heading">Please sign in</h3></legend>
-					<input class="form-control" id="name" type="text" name="name" placeholder="name" required autofocus>
-					<input class="form-control" id="username" type="text" name="username" placeholder="username" required>
+					<input class="form-control" id="name" type="text" name="name" placeholder="name" value="<?php echo $_SESSION['temp_name'];$_SESSION['temp_name'] = ""; ?>" required autofocus>
+					<input class="form-control" id="username" type="text" name="username" placeholder="username" value="<?php echo $_SESSION['temp_username'];$_SESSION['temp_username'] = ""; ?>" required>
 					<input class="form-control" id="password" type="password" name="password" placeholder="password" required>
 					<input class="form-control" id="re-password" type="password" name="re-password" placeholder="password" required>
 					<br>
